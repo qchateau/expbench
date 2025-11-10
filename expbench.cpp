63dc1955733a524b8065efaddfa8ed5f85b85508
Quentin Chateau <quentin.chateau@gmail.com>
2025-11-10 12:06:11 +0100

    Update

--- expbench.cpp
+++ expbench.cpp
@@ -16,61 +16,134 @@ std::vector<T> make_data(std::size_t size, double min = -50, double max = 50)
     return data;
 }
 
+void inner_loop(auto const& data, auto& output, auto exp)
+{
+    for (auto i = 0ul; i < data.size(); ++i) {
+        output[i] = exp(data[i]);
+    }
+    benchmark::DoNotOptimize(output);
+}
+
+extern "C" {
+
+[[gnu::noinline]] void inner_loop_float(
+    std::vector<float> const& data,
+    std::vector<float>& output)
+{
+    inner_loop(data, output, [](auto x) { return std::exp(x); });
+}
+
+[[gnu::noinline]] void inner_loop_float_fast(
+    std::vector<float> const& data,
+    std::vector<float>& output)
+{
+    inner_loop(data, output, [](auto x) { return fast_exp(x); });
+}
+
+[[gnu::noinline]] void inner_loop_float_very_fast(
+    std::vector<float> const& data,
+    std::vector<float>& output)
+{
+    inner_loop(data, output, [](auto x) { return very_fast_exp(x); });
+}
+
+[[gnu::noinline]] void inner_loop_double(
+    std::vector<double> const& data,
+    std::vector<double>& output)
+{
+    inner_loop(data, output, [](auto x) { return std::exp(x); });
+}
+
+[[gnu::noinline]] void inner_loop_double_as_float(
+    std::vector<double> const& data,
+    std::vector<double>& output)
+{
+    inner_loop(data, output, [](auto x) {
+        return std::exp(static_cast<float>(x));
+    });
+}
+
+[[gnu::noinline]] void inner_loop_double_fast(
+    std::vector<double> const& data,
+    std::vector<double>& output)
+{
+    inner_loop(data, output, [](auto x) { return fast_exp(x); });
+}
+
+[[gnu::noinline]] void inner_loop_double_very_fast(
+    std::vector<double> const& data,
+    std::vector<double>& output)
+{
+    inner_loop(data, output, [](auto x) { return very_fast_exp(x); });
+}
+}
+
 template <typename T>
-void BM_exp(benchmark::State& state, auto exp)
+void BM_exp(benchmark::State& state, auto inner_loop)
 {
     auto width = state.range(0);
     auto data = make_data<T>(width);
+    auto output = data;
     for (auto _ : state) {
-        for (auto& x : data) {
-            benchmark::DoNotOptimize(exp(x));
-        }
+        inner_loop(data, output);
     }
     state.SetItemsProcessed(state.iterations() * data.size());
 }
 
 void BM_exp_float(benchmark::State& state)
 {
-    BM_exp<float>(state, [](auto x) { return std::exp(x); });
+    BM_exp<float>(state, [](auto const& data, auto& output) {
+        inner_loop_float(data, output);
+    });
 }
-BENCHMARK(BM_exp_float)->Range(1, 4096)->RangeMultiplier(8);
+BENCHMARK(BM_exp_float)->RangeMultiplier(2)->Range(1, 256);
 
-void BM_exp_fast_float(benchmark::State& state)
+void BM_exp_float_fast(benchmark::State& state)
 {
-    BM_exp<float>(state, [](auto x) { return fast_exp(x); });
+    BM_exp<float>(state, [](auto const& data, auto& output) {
+        inner_loop_float_fast(data, output);
+    });
 }
-BENCHMARK(BM_exp_fast_float)->Range(1, 4096)->RangeMultiplier(8);
+BENCHMARK(BM_exp_float_fast)->RangeMultiplier(2)->Range(1, 256);
 
-void BM_exp_very_fast_float(benchmark::State& state)
+void BM_exp_float_very_fast(benchmark::State& state)
 {
-    BM_exp<float>(state, [](auto x) { return very_fast_exp(x); });
+    BM_exp<float>(state, [](auto const& data, auto& output) {
+        inner_loop_float_very_fast(data, output);
+    });
 }
-BENCHMARK(BM_exp_very_fast_float)->Range(1, 4096)->RangeMultiplier(8);
+BENCHMARK(BM_exp_float_very_fast)->RangeMultiplier(2)->Range(1, 256);
 
 void BM_exp_double(benchmark::State& state)
 {
-    BM_exp<double>(state, [](auto x) { return std::exp(x); });
-}
-BENCHMARK(BM_exp_double)->Range(1, 4096)->RangeMultiplier(8);
-
-void BM_exp_float_double(benchmark::State& state)
-{
-    BM_exp<double>(state, [](auto x) {
-        return static_cast<double>(std::exp(static_cast<float>(x)));
+    BM_exp<double>(state, [](auto const& data, auto& output) {
+        inner_loop_double(data, output);
     });
 }
-BENCHMARK(BM_exp_float_double)->Range(1, 4096)->RangeMultiplier(8);
+BENCHMARK(BM_exp_double)->RangeMultiplier(2)->Range(1, 256);
 
-void BM_exp_fast_double(benchmark::State& state)
+void BM_exp_double_as_float(benchmark::State& state)
 {
-    BM_exp<double>(state, [](auto x) { return fast_exp(x); });
+    BM_exp<double>(state, [](auto const& data, auto& output) {
+        inner_loop_double_as_float(data, output);
+    });
 }
-BENCHMARK(BM_exp_fast_double)->Range(1, 4096)->RangeMultiplier(8);
+BENCHMARK(BM_exp_double_as_float)->RangeMultiplier(2)->Range(1, 256);
 
-void BM_exp_very_fast_double(benchmark::State& state)
+void BM_exp_double_fast(benchmark::State& state)
 {
-    BM_exp<double>(state, [](auto x) { return very_fast_exp(x); });
+    BM_exp<double>(state, [](auto const& data, auto& output) {
+        inner_loop_double_fast(data, output);
+    });
 }
-BENCHMARK(BM_exp_very_fast_double)->Range(1, 4096)->RangeMultiplier(8);
+BENCHMARK(BM_exp_double_fast)->RangeMultiplier(2)->Range(1, 256);
+
+void BM_exp_double_very_fast(benchmark::State& state)
+{
+    BM_exp<double>(state, [](auto const& data, auto& output) {
+        inner_loop_double_very_fast(data, output);
+    });
+}
+BENCHMARK(BM_exp_double_very_fast)->RangeMultiplier(2)->Range(1, 256);
 
 BENCHMARK_MAIN();
